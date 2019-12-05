40184359c0bcf5dc2971b23f92dd1a35431cb7a1
Wellesley Yu <byu@lcs.on.ca>
2019-12-05 14:50:29 -0500

    Connected view to controller

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionNumber: UITextField!
+    
+    @IBOutlet weak var studentAnswer: UITextField!
+    
+    @IBOutlet weak var correctAnswers: UITextField!
+    
+    @IBOutlet weak var outputCheck: UITextView!
+    
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func checkAnswer(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
