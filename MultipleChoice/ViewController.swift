3cec880a9702ab551d26b8e2d02ec9e13a81c150
Wellesley Yu <byu@lcs.on.ca>
2019-12-05 14:23:52 -0500

    Initial Commit

---
+++ MultipleChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleChoice
+//
+//  Created by Yu, Boning on 2019-12-05.
+//  Copyright © 2019 Wellesley Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
